dfc145b3d9d143ce749b841ee140e656d957c609
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2025-03-16 15:09:10 +0100

    ajotu du

--- Kiana_Kaslana_finder.py
+++ Kiana_Kaslana_finder.py
@@ -1,5 +1,4 @@
 import os 
-import cv2 as cv
 from test_pre_train_model.ssd_anime_face_detect import ssd_anime_face_detect
 
 
@@ -7,8 +6,5 @@ def get_nb_face_found(directory):
     for filename in os.listdir(directory):
         img_path = os.path.join(directory, filename)
         head = ssd_anime_face_detect(img_path,"test_pre_train_model\ssd_anime_face_detect.pth")
-
-        
-
 
 print(get_nb_face_found("imgs_test\kiana"))

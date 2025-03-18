a5e89a915dbb31538d3ff2d8a1fefe173006d30a
ssea2 <ssea2.heal@gmail.com>
2025-03-18 18:50:47 +0100

    t

--- Kiana_Kaslana_finder.py
+++ Kiana_Kaslana_finder.py
@@ -4,10 +4,12 @@ from test_pre_train_model.ssd_anime_face_detect import ssd_anime_face_detect
 
 
 def get_nb_face_found(directory):
+    nohead+=1
     for filename in os.listdir(directory):
         img_path = os.path.join(directory, filename)
         head = ssd_anime_face_detect(img_path,"test_pre_train_model\ssd_anime_face_detect.pth")
-
+    if head == []:
+        nohead +=1
         
 
 

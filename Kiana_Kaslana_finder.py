ad5522f5e2dd72b76fcb2ab2572a4cfcd40a4de9
ssea2 <ssea2.heal@gmail.com>
2025-04-18 16:41:07 +0200

    t

--- Kiana_Kaslana_finder.py
+++ Kiana_Kaslana_finder.py
@@ -1,4 +1,4 @@
-import os 
+"""import os 
 from test_pre_train_model.ssd_anime_face_detect import ssd_anime_face_detect
 
 def get_nb_face_found(directory):
@@ -9,4 +9,18 @@ def get_nb_face_found(directory):
         if head == []:
             nohead +=1
 
-print(get_nb_face_found("imgs_test\kiana"))
+print(get_nb_face_found("imgs_test\kiana"))"""
+
+# import libraries
+from yoloface import face_analysis
+import numpy
+import cv2
+face=face_analysis()        #  Auto Download a large weight files from Google Drive.
+                            #  only first time.
+                            #  Automatically  create folder .yoloface on cwd.
+# example 1
+#%%time
+img,box,conf=face.face_detection(image_path='imgs_test\kiana\kiana_2.jpg',model='tiny')
+print(box)                  # box[i]=[x,y,w,h]
+print(conf)                 #  value between(0 - 1)  or probability
+face.show_output(img,box)
